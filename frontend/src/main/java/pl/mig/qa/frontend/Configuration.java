936ad993f18b266f120c4de04fcbbe66fcef8c0a
Mistrz007 <michalski.m.lukasz@gmail.com>
2018-10-17 11:57:58 +0200

    copmmit 222

--- frontend/src/main/java/pl/mig/qa/frontend/Configuration.java
+++ frontend/src/main/java/pl/mig/qa/frontend/Configuration.java
@@ -9,6 +9,9 @@ public class Configuration {
     private static final String ENVIRONMENT = CONFIG.getString("environment");
     private static final Config ENV = CONFIG.getConfig("environments").getConfig(ENVIRONMENT);
 
+
+
+
     public static final String WORDPRESS_URL = ENV.getString("baseURL");
     public static final String LOGIN = ENV.getString("login");
     public static final String PASSWORD = ENV.getString("password");
