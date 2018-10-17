fa4075eca5b431fecdfd756343142d75e75cd2c1
Mistrz007 <michalski.m.lukasz@gmail.com>
2018-10-17 08:45:00 +0200

    copmmit 222

--- frontend/src/test/java/pl/mif/qa/frontend/factory/FrontEndTestSelenium.java
+++ frontend/src/test/java/pl/mif/qa/frontend/factory/FrontEndTestSelenium.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FrontEndTest extends FrontConfiguration {
+public class FrontEndTestSelenium extends FrontConfiguration {
 
     @Test
     public void firstFrontTest(){
